0fe994e4a39637063f54f56ab5fd7b8cc5b09ebd
Brian Dunnette <brian@dunnette.us>
2015-04-10 10:06:54 -0500

    adding cornerstone JS/CSS

---
+++ package.js
@@ -0,0 +1,26 @@
+Package.describe({
+  name: 'bdunnette:cornerstone',
+  version: '0.0.1',
+  // Brief, one-line summary of the package.
+  summary: 'Cornerstone medical imaging library packaged for Meteor',
+  // URL to the Git repository containing the source code for this package.
+  git: 'https://github.com/bdunnette/meteor-cornerstone.git',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md',
+  license: 'MIT'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.1.0.2');
+  api.addFiles([
+'lib/cornerstone.js',
+'styles/cornerstone.css'
+],'client');
+});
+
+Package.onTest(function(api) {
+  api.use('tinytest');
+  api.use('bdunnette:cornerstone');
+  api.addFiles('cornerstone-tests.js');
+});
